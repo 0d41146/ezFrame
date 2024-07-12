4ee51245a5075cee1f0137d95402ba6c372f6ecb
0d41146 <41146f@gmail.com>
2024-07-12 17:10:10 +0900

    added PC input

--- proj/proj1/src/main.c
+++ proj/proj1/src/main.c
@@ -1,60 +1,61 @@
-#include <console.h>
-#include <generated/csr.h>
-#include <generated/mem.h>
-#include <generated/soc.h>
-#include <stdio.h>
-
-#include <irq.h>
-#include <libbase/uart.h>
-#include <libbase/console.h>
+#include "proj_menu.h"
+#include "perf.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <assert.h>
 
-#include "riscv.h"
+#include "cfu.h"
+#include "menu.h"
 
 #define NAME "qn24b base"
 #define VER  "version 1.0.1 2004-09-02"
-#define MAX 32 /** 32 is a real max! **/
+#define MAX 29 /** 32 is a real max! **/
 #define MIN 2
 #define N   16
 
-#define CUSTOM0 0x0B
-#define cfu_init(x)   opcode_R(CUSTOM0, 0, 0, x, 0)
-#define cfu_kernel()  opcode_R(CUSTOM0, 1, 0, 0, 0)
-#define cfu_get_ret() opcode_R(CUSTOM0, 2, 0, 0, 0)
+#define cfu_init(x)       (cfu_op0(0, x, 0))
+#define cfu_kernel()      (cfu_op1(0, 0, 0))
+#define cfu_get_ret()     (cfu_op2(0, 0, 0))
+#define cfu_get_PC()    (cfu_op3(0, 0, 0))
+#define cfu_check_rr()    (cfu_op4(0, 0, 0))
+#define cfu_check_a_cdt() (cfu_op5(0, 0, 0))
+#define cfu_check_a_col() (cfu_op6(0, 0, 0))
+#define cfu_check_a_pos() (cfu_op7(0, 0, 0))
 
+int h = 0;
+int r = 0;
+int ret = 0;
+
+namespace {
 void queens(){
   int i;
   long long answers = 0;
 
   printf("The size of N: %d\n", N);
   
+
   for(i=0; i<(N/2+N%2); i++){
     cfu_init(i);
     while(cfu_kernel());
-    int ret = cfu_get_ret();
+    ret = cfu_get_ret();
+    printf("PC: 0x%lx\n", cfu_get_PC());
     answers += ret;
     if(i!=N/2) answers += ret;
   }
 
   printf("%lld\n", answers);
-  printf("---\n");
 }
 
-int main(void) {
-#ifdef CONFIG_CPU_HAS_INTERRUPT
-	irq_setmask(0);
-	irq_setie(1);
-#endif
-	uart_init();
-  
+struct Menu MENU = {
+    "Project Menu",
+    "project",
+    {
+        MENU_ITEM('0', "N-queens", queens),
+        MENU_END,
+    },
+};
 
+};  // anonymous namespace
 
-  queens();
-
-  printf("===END===\n");
-  return 0;
-}
+extern "C" void do_proj_menu() { menu_run(&MENU); }
--- soc/cfu.v
+++ soc/cfu.v
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 `define MAX 31
 `define MIN 2
 `define N   16
@@ -22,6 +21,7 @@ module Cfu (
   input      [9:0]    cmd_payload_function_id,
   input      [31:0]   cmd_payload_inputs_0,
   input      [31:0]   cmd_payload_inputs_1,
+  input      [31:0]   cmd_payload_inputs_2,
   output              rsp_valid,
   input               rsp_ready,
   output     [31:0]   rsp_payload_outputs_0,
@@ -33,14 +33,19 @@ module Cfu (
   assign rsp_valid = cmd_valid;
   assign cmd_ready = rsp_ready;
 
+  wire cfu_init     = cmd_payload_function_id == 0;
+  wire cfu_kernel   = cmd_payload_function_id == 1;
+  wire cfu_get_ret  = cmd_payload_function_id == 2;
+  wire cfu_get_PC_value = cmd_payload_function_id == 3;
+
   reg [31:0] a_cdt [0:`MAX]; /* candidates        */
   reg [31:0] a_col [0:`MAX]; /* column            */
   reg [31:0] a_pos [0:`MAX]; /* positive diagonal */
   reg [31:0] a_neg [0:`MAX]; /* negative diagonal */
   
-  reg [31:0] r_h    = 0;       /* height or level  */
-  reg [31:0] r_r    = 0;       /* candidate vector */
-  reg [31:0] r_ret  = 0;       /* return value      */
+  reg [31:0] reg_h    = 0;       /* height or level  */
+  reg [31:0] reg_r    = 0;       /* candidate vector */
+  reg [31:0] reg_ret  = 0;       /* return value      */
 
   integer i;
   initial begin
@@ -51,51 +56,47 @@ module Cfu (
       a_neg[i] = 0;
     end
   end
-  
-  wire        cfu_init        = (cmd_payload_function_id == 0);
-  wire        cfu_kernel      = (cmd_payload_function_id == 1);
-  wire        cfu_get_ret     = (cmd_payload_function_id == 2);
 
-  wire        w_bool          = ~(r_r == 0 && r_h - 1 == 0);
-  wire [31:0] lsb             = (-r_r) & r_r;
+  wire        w_bool      = ~(reg_r == 0 && reg_h == 1);
+  wire [31:0] lsb1        = (~reg_r + 1) & reg_r;
 
   always @(posedge clk) begin
     if (cmd_valid) begin
       if (cfu_init) begin
-        r_h       <= 1;
-        r_r       <= (1 << cmd_payload_inputs_0);
+        reg_ret   <= 0;
+        reg_h     <= 1;
+        reg_r     <= 1 << (cmd_payload_inputs_0);
         a_col[1]  <= (1 << `N) - 1;
         a_pos[1]  <= 0;
         a_neg[1]  <= 0;
-        r_ret     <= 0;
       end
 
-      if (cfu_kernel) begin
-        if (r_r) begin
-          a_cdt[r_h + 1] <= r_r & ~lsb;
-          a_col[r_h + 1] <= a_col[r_h] & ~lsb;
-          a_pos[r_h + 1] <= (a_pos[r_h] | lsb) << 1;
-          a_neg[r_h + 1] <= (a_neg[r_h] | lsb) >> 1;
-          r_r <= (a_col[r_h] & ~lsb) &
-                ~(((a_pos[r_h] | lsb) << 1) |
-                  ((a_neg[r_h] | lsb) >> 1));
-          r_h <= r_h + 1;
+      else if (cfu_kernel) begin
+        if (reg_r) begin
+          a_cdt[reg_h+1] <= (          reg_r & ~lsb1);
+          a_col[reg_h+1] <= (a_col[reg_h] & ~lsb1);
+          a_pos[reg_h+1] <= ((a_pos[reg_h] |  lsb1) << 1);
+          a_neg[reg_h+1] <= ((a_neg[reg_h] |  lsb1) >> 1);
+
+          reg_r <= (a_col[reg_h] & ~lsb1) &
+                  ~(((a_pos[reg_h] | lsb1) << 1) |
+                    ((a_neg[reg_h] | lsb1) >> 1));
+          reg_h <= reg_h + 1;
         end else begin
-          r_r <= a_cdt[r_h];
-          r_h <= r_h - 1;
-          if (r_h - 1 == `N) begin
-            r_ret <= r_ret + 1;
-          end
+          if (reg_h == `N + 1) reg_ret <= reg_ret + 1;
+          reg_r <= a_cdt[reg_h];
+          reg_h <= reg_h - 1;
         end
       end
     end
   end
+ 
 
   //
   // select output -- note that we're not fully decoding the 3 function_id bits
   //
-  assign rsp_payload_outputs_0 = (cfu_init)         ? 0 :
-                                 (cfu_kernel)       ? w_bool :
-                                 (cfu_get_ret)      ? r_ret : -1;
-
+  assign rsp_payload_outputs_0 = (cfu_init)         ? 0                    :
+                                 (cfu_kernel)       ? w_bool               :
+                                 (cfu_get_ret)      ? reg_ret              :
+                                 (cfu_get_PC_value) ? cmd_payload_inputs_2 : -1;
 endmodule
